20acf0eb8eba5b27a818671ef88e03788ab8b181
FLo-ABB <florian.lobert@fr.abb.com>
2024-03-21 17:08:55 +0100

    Update package configuration in setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ setup(
     name='CognexNativePy',
     version='1.0.0',
     description='A Python library for communicating with Cognex In-Sight vision systems. Wrapper of the native commands',
-    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Florian LOBERT',
     url='https://github.com/FLo-ABB/CognexNativePy',
-    packages=find_packages(where='src'),
+    packages=find_packages(where=''),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
