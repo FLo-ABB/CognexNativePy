c5daf2d247dbe0e3926d91048c0b31faad834b9e
FLo-ABB <florian.lobert@fr.abb.com>
2024-03-21 16:42:17 +0100

    create file to package to pypi

---
+++ src/CognexNativePy/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CognexNativePy',
+    version='1.0.0',
+    description='A Python library for communicating with Cognex In-Sight vision systems. Wrapper of the native commands',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Florian LOBERT',
+    url='https://github.com/FLo-ABB/CognexNativePy',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src/CognexNativePy'},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+    ],
+)
