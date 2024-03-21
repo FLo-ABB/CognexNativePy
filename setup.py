ce0ed4d1657d1753d394f60fc183cff8c6a23551
FLo-ABB <florian.lobert@fr.abb.com>
2024-03-21 17:11:41 +0100

    Update setup.py with long description

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='CognexNativePy',
-    version='1.0.0',
     description='A Python library for communicating with Cognex In-Sight vision systems. Wrapper of the native commands',
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Florian LOBERT',
     url='https://github.com/FLo-ABB/CognexNativePy',
